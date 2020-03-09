0d99b5f7a5d0f8f012cd331225598a12b270eb1b
wtrawlings <rawlings.warren@gmail.com>
2020-03-08 20:43:04 -0400

    added axios

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Axios from 
+import axios from "axios";
 class App extends Component {
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log(response);
+        this.setState({smurfs: response.data})
+      })
+      .catch(console.log("error"));
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,6 +19,7 @@ class App extends Component {
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
       </div>
+
     );
   }
 }
